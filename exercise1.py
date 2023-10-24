8b36c347e5f105b8dac0ce2617316d43e57dd36e
jonathan <jonathan.davies@dci-student.org>
2023-10-24 16:02:29 +0200

    Complete_exercise

---
+++ exercise1.py
@@ -0,0 +1,26 @@
+# Variables
+var_a = 1500 + 3000
+var_b = 1500 + 3000.00
+var_c = 1000 / 5
+var_d = 1200 + 200
+var_e = 3004 + 996
+
+# Printing var_a and var_b
+print("var_a:", var_a)
+print("var_b:", var_b)
+
+# The difference between var_a and var_b is in their data types
+# var_a is an integer, and var_b is a float
+
+# type of var_c
+print("var_c is of type:", type(var_c))
+
+# var_c is an int bc it's the result of an integer div
+
+ print var_d would result in an error 
+# This would make an error
+print("var_d:", var_d)
+
+# The value of var_e and its object type
+print("var_e:", var_e)
+print("var_e is of type:", type(var_e))
---
+++ exercise3.py
@@ -0,0 +1,29 @@
+# Expressions whether true or false
+
+# 5 is greater than 4 and 3 is equal to 5
+
+# The answer is false with an AND 
+
+# not (5 > 4)
+
+# The initial expression is true because 5 is greater than 4.
+# BUT because its a NOT  This means it is FALSE
+
+ #(5 > 4) or (3 == 5)
+ # first is true second is false
+ # but the answer is TRUE
+
+ #not ((5 > 4) or (3 == 5))
+ # The two expressions in brackets are TRUE, BUT
+ # The NOT at the beginning means the overall expression is
+ #FALSE
+
+  #(True and True) and (True == False)
+
+#This is an AND expression. So: first expression is TRUE
+#Second is FALSE
+#So both conditions must be TRUE but they arent so:
+#The expression is FALSE
+
+#(not False) or (not True)
+
